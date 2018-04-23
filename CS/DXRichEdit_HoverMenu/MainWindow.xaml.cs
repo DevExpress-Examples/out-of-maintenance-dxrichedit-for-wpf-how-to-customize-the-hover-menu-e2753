aab82b171080f5cc0274b0c3b7749abc5858e03a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXRichEdit_HoverMenu/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿#region #usings
+using System.Windows;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.RichEdit;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.Commands;
+using DevExpress.XtraRichEdit.Menu;
+using DevExpress.Xpf.RichEdit.Menu;
+#endregion #usings
+
+
+namespace DXRichEdit_HoverMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            richEditControl1.Loaded += new RoutedEventHandler(richEditControl1_Loaded);
+            richEditControl1.HoverMenuShowing += new HoverMenuShowingEventHandler(richEditControl1_HoverMenuShowing);
+        }
+
+
+        void richEditControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+            richEditControl1.LoadDocument("sample_document.docx", DocumentFormat.OpenXml);
+        }
+
+        #region #hovermenushowing
+        void richEditControl1_HoverMenuShowing(object sender, HoverMenuShowingEventArgs e)
+        {
+            // Remove DecreaseIndent and IncreaseIndent command menu items.
+            for (int i = 0; i < e.Menu.ItemsCount; i++) {
+                BarItemLink bLink = (BarItemLink)e.Menu.ItemLinks[i];
+                RichEditUICommand cmd = (RichEditUICommand)bLink.Item.Command;
+                if (cmd.CommandId == RichEditCommandId.DecreaseIndent 
+|| cmd.CommandId == RichEditCommandId.IncreaseIndent)
+                    e.Menu.ItemLinks.Remove(bLink); 
+            }
+            // Add a separator.
+            BarItemLinkSeparator itemSep = new BarItemLinkSeparator();
+            e.Menu.ItemLinks.Add(itemSep);
+            // Create a new menu item and add it to the hover menu.
+            RichEditMenuItem item1 = new RichEditMenuItem();
+            item1.Content = "Replace Emoticon with Smiley";
+            MySmileyCommand cmd_toAdd = new MySmileyCommand(richEditControl1);
+            item1.Command = cmd_toAdd;
+            item1.Glyph = cmd_toAdd.Glyph;
+            e.Menu.ItemLinks.Add(item1);
+        }
+        #endregion #hovermenushowing
+    }
+    }
---
+++ VB/DXRichEdit_HoverMenu/MySmileyCommand.vb
@@ -0,0 +1,90 @@
+﻿#Region "#usingsincommands"
+Imports System
+Imports System.Drawing
+Imports System.IO
+Imports System.Reflection
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.API.Native
+#End Region ' #usingsincommands
+
+Namespace DXRichEdit_HoverMenu
+    Friend Class MySmileyCommand
+        Implements ICommand
+
+
+        Private ReadOnly control_Renamed As IRichEditControl
+
+        Private ReadOnly glyph_Renamed As ImageSource
+        Private ReadOnly smile As Image
+
+        Public Sub New(ByVal control As IRichEditControl)
+            Me.control_Renamed = control
+            Me.glyph_Renamed = CreateImageSourceFromResx("smile.png")
+            Me.smile = CreateImageFromResx("smile.png")
+        End Sub
+
+
+        Public ReadOnly Property Glyph() As ImageSource
+            Get
+                Return glyph_Renamed
+            End Get
+        End Property
+
+
+        Private Function CreateImageSourceFromResx(ByVal name As String) As ImageSource
+            Dim assembly As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly()
+            Dim bi As New BitmapImage()
+            Using stream As Stream = assembly.GetManifestResourceStream("DXRichEdit_HoverMenu.Images." & name)
+                bi.BeginInit()
+                bi.StreamSource = stream
+                bi.EndInit()
+            End Using
+            Return CType(bi, ImageSource)
+        End Function
+
+        Private Function CreateImageFromResx(ByVal name As String) As Image
+            Dim im As Image
+            Dim assembly As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly()
+            Using stream As Stream = assembly.GetManifestResourceStream("DXRichEdit_HoverMenu.Images." & name)
+                im = Image.FromStream(stream)
+            End Using
+            Return im
+        End Function
+
+        Protected Friend ReadOnly Property Control() As IRichEditControl
+            Get
+                Return Me.control_Renamed
+            End Get
+        End Property
+
+
+        Public Function CanExecute(ByVal parameter As Object) As Boolean Implements ICommand.CanExecute
+            Return True
+        End Function
+
+        Public Custom Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
+            AddHandler(ByVal value As EventHandler)
+            End AddHandler
+            RemoveHandler(ByVal value As EventHandler)
+            End RemoveHandler
+            RaiseEvent(ByVal sender As System.Object, ByVal e As System.EventArgs)
+            End RaiseEvent
+        End Event
+
+        Public Sub Execute(ByVal parameter As Object) Implements ICommand.Execute
+'            #Region "#findall"
+    Dim doc As Document = Control.Document
+    Dim range As DocumentRange = doc.Selection
+    Dim smileyRanges() As DocumentRange = doc.FindAll(":-)", SearchOptions.WholeWord, range)
+    For Each r As DocumentRange In smileyRanges
+        doc.Delete(r)
+        doc.InsertImage(r.Start, smile)
+    Next r
+'            #End Region ' #findall
+        End Sub
+
+    End Class
+End Namespace
